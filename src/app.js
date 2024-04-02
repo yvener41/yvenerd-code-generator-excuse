abe708ed727f7826e0459c661bf594d81f9d373e
Yvener Dutervil <72567854+yvener41@users.noreply.github.com>
2024-04-02 01:25:22 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // 1. Create the "parts of the sentence" arrays
+  let who = ["The dog", "My grandma", "The mailman", "The bird"];
+  let action = ["ate", "threw away", "mailed", "stole"];
+  let what = ["my homework", "my cellphone", "the car"];
+  let when = ["yesterday", "this morning", "last week !"];
+  // 2. combine random elements in each array to form a sentence
+  // create a function to randomly select an element in an array
+  function selectArrayElement(arrayName, arrayLength) {
+    // 1. Generate a random number based on the length of arrayName
+    let randomNumber = Math.floor(Math.random() * arrayLength);
+
+    // 2. Assign an element from the arrayName to be returned
+    let selectedElement = arrayName[randomNumber];
+    return selectedElement;
+  }
+
+  let randomWho = selectArrayElement(who, who.length);
+  let randomAction = selectArrayElement(action, action.length);
+  let randomWhat = selectArrayElement(what, what.length);
+  let randomWhen = selectArrayElement(when, when.length);
+
+  // string templates
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  /* 3. print the excuse on the terminal, and then in the browser
+  console.log(excuse); */
+
+  //retrieving html elements to manipulate
+  const htmlElement = document.querySelector(".excuse");
+  htmlElement.innerHTML = excuse;
 };
